1171b4585735d13416d3df38c3a8534669f1a605
Justin <justin.studer@bluewin.ch>
2024-10-31 00:51:07 +0100

    Finished basic lighting exercises

--- OpenGLTutorial/fragment_shader.glsl
+++ OpenGLTutorial/fragment_shader.glsl
@@ -1,14 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoord;
+in vec3 LightingColor;
 
-// texture samplers
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+uniform vec3 objectColor;
 
 void main()
 {
-	// linearly interpolate between both textures (80% container, 20% awesomeface)
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2f);
+	FragColor = vec4(LightingColor * objectColor, 1.0);
 }
