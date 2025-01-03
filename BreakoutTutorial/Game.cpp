290447eacb834b291ea83386142142c25fadf3e2
Justin <justin.studer@bluewin.ch>
2025-01-03 16:15:24 +0100

    WIP sprite rendering

---
+++ BreakoutTutorial/Game.cpp
@@ -0,0 +1,43 @@
+﻿#include "Game.h"
+
+#include "ResourceManager.h"
+
+Game::Game(unsigned int width, unsigned int height) : State(GAME_ACTIVE), Keys(), Width(width), Height(height) {
+	
+}
+
+Game::~Game() {
+	delete Renderer;
+}
+
+void Game::Init() {
+	// compile shaders
+	ResourceManager::LoadShader("shaders/sprite.vert", "shaders/sprite.frag", nullptr, "sprite");
+
+	// prepare projection matrix
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width), static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+
+	// set uniforms
+	ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+	ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+
+	// set render-specific controls
+	Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+
+	// load textures
+	ResourceManager::LoadTexture("resources/textures/awesomeface.png", true, "face");
+
+}
+
+void Game::Update(float dt) {
+	
+}
+
+void Game::ProcessInput(float dt) {
+	
+}
+
+void Game::Render() {
+	Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(0.0f, 0.0f), glm::vec2(800.f, 600.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
---
+++ BreakoutTutorial/Game.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "SpriteRenderer.h"
+
+// Represents the current state of the game
+enum GameState {
+	GAME_ACTIVE,
+	GAME_MENU,
+	GAME_WIN
+};
+
+class Game
+{
+public:
+	GameState State;
+	bool Keys[1024];
+	unsigned int Width, Height;
+	SpriteRenderer* Renderer;
+	Game(unsigned int width, unsigned int height);
+	~Game();
+
+	void Init();
+
+	void ProcessInput(float dt);
+	void Update(float dt);
+	void Render();
+};
